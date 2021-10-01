2f4dd7321df33f6e1c9a7a2ec428cb71eb7f577a
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-09-30 22:10:46 -0700

    uploading changes to index, script and style

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.getElementById("clear").addEventListener("click", function () {
+  ["upCase", "lowCase", "numbers", "spec-char"].forEach(function(id) {
+    document.getElementById(id).checked = false;
+  });
+  return false;
+})
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -6,6 +13,10 @@ var specialChar = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "
 var char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
 "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//var passGen = window.confirm("Choose password options, then click generate password")
+
+
 
 // function to generate password based on user inputs
 // concatenate variables above based on user choices
@@ -18,10 +29,9 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
